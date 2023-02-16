b89f2c17e6031cbd2449a18da7466cfa98bd073d
Frempong <samankwaa.frempong@gmail.com>
2023-02-16 23:49:49 +0000

    Updated getScores funtion with Async and Await

--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -1,5 +1,7 @@
-const getScore = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HX42ejstWWGWDKZzkjqD/scores/')
-  .then((response) => response.json())
-  .then((data) => data);
+const getScore = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HX42ejstWWGWDKZzkjqD/scores/');
+  const data = await response.json();
+  return data;
+};
 
 export default getScore;
