854285c0a86af98a44e2700cd68738b3d74d71b0
Frempong <samankwaa.frempong@gmail.com>
2023-02-16 22:22:09 +0000

    Update app with API fetching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import displayDOM from './modules/displayDOM.js';
+import formSubmit from './modules/form.js';
+import refresh from './modules/refresh.js';
+
+displayDOM();
+formSubmit();
+refresh();
