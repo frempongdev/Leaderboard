a4d97def24ed4df71c4f41a402db55f6a5c7e425
Frempong <samankwaa.frempong@gmail.com>
2023-02-17 10:35:08 +0000

    Add style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './style.css';
 import displayDOM from './modules/displayDOM.js';
 import formSubmit from './modules/form.js';
 import refresh from './modules/refresh.js';
+import background from './modules/background.js';
 
 displayDOM();
 formSubmit();
 refresh();
+background();
